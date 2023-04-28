ba0faa66ba019dd4ff5aa2c6077f8b38bbdfc9dc
Dennis290699 <trujilloadrian29@gmail.com>
2023-04-28 12:43:59 -0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,40 +1,31 @@
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
+const scoreElement = document.querySelector('.score');
+let score = 0;
 
-const jump = () =>{
+const jump = () => {
     mario.classList.add('jump');
-
     setTimeout(() => {
         mario.classList.remove('jump');
-        }, 500);
+    }, 500);
+};
 
-}
-
-const loop = setInterval( () =>{
-
-    console.log('loop')
-
+const loop = setInterval(() => {
+    score++;
+    scoreElement.textContent = `Score: ${score}`;
     const pipePosition = pipe.offsetLeft;
     const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
 
-    console.log(marioPosition);
-
-    if(pipePosition <= 120 && pipePosition>0 && marioPosition < 80){
-
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
         pipe.style.animation = 'none';
         pipe.style.left = `${pipePosition}px`;
-
         mario.style.animation = 'none';
         mario.style.bottom = `${marioPosition}px`;
-
         mario.src = './images/game-over.png';
-        mario.style.width = '75px'
-        mario.style.marginLeft = '50px'
-
+        mario.style.width = '75px';
+        mario.style.marginLeft = '50px';
         clearInterval(loop);
-
     }
-
-}, 10);
+}, 50);
 
 document.addEventListener('keydown', jump);
